35927c5b6ce514f56fb12cdf6e0422b5e7c066e4
João Azevedo <joao.azevedo@ufv.br>
2021-04-03 19:05:56 -0300

    MAINPAGE adicionada

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import papaSmurf from './smurf.jpg';
+import './style.css'
 
 function Main() {
     return(
-        <>
-            <h1>
-                Main
-            </h1>
-        </>
+        <main>
+            <section className="main-welcome-cta">
+                <header className="main-welcome-cta-header">
+                    <h1>Hi, I'm a ReactJS + NodeJS blog.</h1>
+                    <p>Here you can read our articles plus create your own. Enjoy and checkout our biography anytime.</p>
+                </header>
+                <button>Learn more</button><br />
+                <img src={papaSmurf} title="Papa Smurf posing for photo" alt="Papa Smurf posing"/>
+            </section>
+        </main>
     );
 }
 
