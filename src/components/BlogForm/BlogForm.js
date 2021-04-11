be889499e2e4808fcc8e1bc90d62b7e39e5b297a
João Azevedo <joao.azevedo@ufv.br>
2021-04-11 19:21:29 -0300

    State adicionado ao componente BlogForm.

--- src/components/BlogForm/BlogForm.js
+++ src/components/BlogForm/BlogForm.js
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import './BlogForm.css'
 
 import api from '../../services/api';
-import { Redirect } from 'react-router';
 
 function BlogForm() {
-    const [blogs, setBlogs] = useState([]);
+    const [form, setForm] = useState({title: '', snippet: '', text: ''});
     
     const { register, handleSubmit, reset } = useForm();
     
+    function handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+
+        const newForm = form;
+        newForm[name] = value;
+        setForm(newForm);
+
+        //console.log("newForm:", newForm);
+        //console.log("form:", form);
+    }
+
     const onSubmit = async (data) => {
         const response = await api.post('blogs', data);
-        const blog = response.data;
-        setBlogs([...blogs, blog]);
-        console.log(blogs);
+        console.log(response.status);
         reset();
     };
+
+    
 
     return(
         <section className='main-blog-form'>
@@ -31,21 +43,25 @@ function BlogForm() {
                         type='text'
                         placeholder='example@mail.com'
                         {...register('title')}
+                        onChange={handleChange}
                         required
                     />
                     <label htmlFor='snippet'>Blog snippet:</label>
                     <input
                         type='text'
                         {...register('snippet')}
+                        onChange={handleChange}
                         required
                     />
                     <textarea
                         {...register('text')}
+                        onChange={handleChange}
                         required 
                     />
                     <input type='Submit' />
                 </form>
             </div>
+            <p>test: {JSON.stringify(form)} </p>
         </section>
     );
 }
