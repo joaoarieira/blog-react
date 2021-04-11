60535134be71e944a702e24154711f8829420ccc
João Azevedo <joao.azevedo@ufv.br>
2021-04-11 14:42:54 -0300

    NEWBLOG funcionando com a API.

--- src/components/BlogForm/BlogForm.js
+++ src/components/BlogForm/BlogForm.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import './BlogForm.css'
 
+import api from '../../services/api';
+import { Redirect } from 'react-router';
+
 function BlogForm() {
+    const [blogs, setBlogs] = useState([]);
+    
+    const { register, handleSubmit, reset } = useForm();
+    
+    const onSubmit = async (data) => {
+        const response = await api.post('blogs', data);
+        const blog = response.data;
+        setBlogs([...blogs, blog]);
+        console.log(blogs);
+        reset();
+    };
+
     return(
         <section className='main-blog-form'>
             <div className='main-blog-form-content'>
                 <header>
                     <h1>New blog</h1>
                 </header>
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor='title'>Blog title:</label>
-                    <input type='text' id='title' required/>
+                    <input 
+                        type='text'
+                        placeholder='example@mail.com'
+                        {...register('title')}
+                        required
+                    />
                     <label htmlFor='snippet'>Blog snippet:</label>
-                    <input type='text' id='snippet' required/>
-                    <textarea id='body' required></textarea>
-                    <button>Submit</button>
+                    <input
+                        type='text'
+                        {...register('snippet')}
+                        required
+                    />
+                    <textarea
+                        {...register('text')}
+                        required 
+                    />
+                    <input type='Submit' />
                 </form>
             </div>
         </section>
